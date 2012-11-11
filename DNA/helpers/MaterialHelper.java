e1c20b9c3af4935ccdc52095d19bb6b541026f20
SparkMaster <chrisbarwick23@hotmail.com>
2012-11-10 21:10:51 -0600

    Tons of Refractoring.. Added Creative Tab.

--- DNA/helpers/MaterialHelper.java
+++
@@ -1,9 +0,0 @@
-package DNA.helpers;
-
-import net.minecraft.src.MapColor;
-import net.minecraft.src.Material;
-
-public class MaterialHelper 
-{
-	public static final Material cell = new Material(MapColor.grassColor);
-}
